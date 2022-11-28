0d36c01f35f0cf00be446c19b1b872e90d041bc7
swyner97 <swyner97@gmail.com>
2022-11-27 19:03:56 -0800

    post post not working

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,3 +1,34 @@
 const router = require('express').Router();
+const { Post } = require('../../models/')
 
+// THEN I am taken to the homepage and presented with existing blog posts that include the post title and the date created
+// WHEN I click on an existing blog post
+// THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+// WHEN I enter a comment and click on the submit button while signed in
+// THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
+
+
+// post a post
+router.post('/', async (req, res) => {
+    const body = req.body;
+
+    try {
+        const newPost = await Post.create({ ...body, userId: req.session.id });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// get all posts
+router.get('/dashboard/posts', async (req, res) => {
+    try {
+        const allPosts = await Post.findAll();
+        res.json(allPosts)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// show single post
 module.exports = router;
--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -21,20 +21,6 @@ router.get('/dashboard', (req, res) => {
 })
 
 
-// // get all posts for homepage
-// router.get('/', async (req, res) => {
-//   try {
-//     const postData = await Post.findAll({
-//       include: [User],
-//     });
-
-//     const posts = postData.map((post) => post.get({ plain: true }));
-
-//     res.render('all-posts', { posts });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 // // get single post
 // router.get('/post/:id', async (req, res) => {
