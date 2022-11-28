bb1158cf8629e4eca91d88169c0c19d2036e714e
swyner97 <swyner97@gmail.com>
2022-11-27 19:34:11 -0800

    working post post

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -13,7 +13,7 @@ router.post('/', async (req, res) => {
     const body = req.body;
 
     try {
-        const newPost = await Post.create({ ...body, userId: req.session.id });
+        const newPost = await Post.create({ ...body, userId: req.session.userId });
         res.status(200).json(newPost);
     } catch (err) {
         res.status(500).json(err)
